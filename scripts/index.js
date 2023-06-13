235b24613185088be5e645cc8d83e11d35401e62
RamazaN <ramazan.abdullaev.00@gmail.com>
2023-06-13 23:17:38 +0300

    4

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,48 @@
 
-let closeMenu = document.querySelector('.header__mobile-burger');
-let mobileMenu = document.getElementById('mobile__menuWrap');
+$(function(){
+	$('.offer').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+})
+
+//$(function(){
+//	$('.news__list').slick({
+//		infinite: true,
+//		slidesToShow: 3,
+//		slidesToScroll: 1,
+//	 });
+//})
+
+
+/////////////////////////// MOBILE MENU  /////////////////////////
+let openMenu = document.querySelector('.header__mobile-burger');
+let closeMenu = document.querySelector('.mobile__menu-close');
+let mobileMenuWrap = document.getElementById('mobile__menuWrap');
+let mobMenu = document.querySelector('.mobile__menu');
+
+
+openMenu.addEventListener('click', function(e){
+	mobileMenuWrap.classList.remove('close');								
+	mobileMenuWrap.classList.add('mobile__menuWrap');
+})
 
 closeMenu.addEventListener('click', function(e){
-	mobileMenu.classList.remove('close');
-	mobileMenu.classList.add('mobile__menuWrap');
+	mobileMenuWrap.classList.remove('mobile__menuWrap');
+	mobileMenuWrap.classList.add('close');
 })
+
+mobileMenuWrap.addEventListener('click', function(e){
+	mobileMenuWrap.classList.remove('mobile__menuWrap');
+	mobileMenuWrap.classList.add('close');
+})
+
+mobMenu.addEventListener('click', function(e){
+   e.stopPropagation()		
+})
+
+
+
+
+
