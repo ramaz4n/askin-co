c7227e0f9fe7c54a2fa5a2ee9778db3d4968df29
RamazaN <ramazan.abdullaev.00@gmail.com>
2023-07-03 19:58:17 +0300

    17

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -64,7 +64,17 @@ const swiperNews = new Swiper('.news-swiper', {
 	loop: true,
 	slidesPerView: 4,
 	slidesPerGroup: 1,
-
+	breakpoints: {
+		320:{
+			slidesPerView: 1,
+		},
+		480:{
+			slidesPerView: 2,
+		},
+		992:{
+			slidesPerView: 4,
+		},
+	}
  });
 
 
